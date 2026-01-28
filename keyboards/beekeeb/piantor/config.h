f116856617409bc0c465d4d3378400c20b546ac4
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2026-01-28 21:27:33 +0100

    hack when right hand tx got zapped

--- keyboards/beekeeb/piantor/config.h
+++ keyboards/beekeeb/piantor/config.h
@@ -7,6 +7,10 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 
-#define SERIAL_USART_FULL_DUPLEX
-#define SERIAL_USART_TX_PIN GP0
-#define SERIAL_USART_RX_PIN GP1
+// #define SERIAL_USART_FULL_DUPLEX
+// when GP0 in right hand got zapped, needed to
+// make the comm between the splits half duplex,
+// and use GP1 for the right hand and GP0 for the left hand
+// as well as make the right hand master.
+#define SERIAL_USART_TX_PIN GP1
+// #define SERIAL_USART_RX_PIN GP1
